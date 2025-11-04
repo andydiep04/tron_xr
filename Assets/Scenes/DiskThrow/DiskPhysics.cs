9bbd424cabe2ab270afd0618ea9027b154a24314
arshakir <arshakir999@gmail.com>
2025-11-04 16:20:35 -0500

    fixed up lift calculations and cleaned up code

--- Assets/Scenes/DiskThrow/DiskPhysics.cs
+++ Assets/Scenes/DiskThrow/DiskPhysics.cs
@@ -4,23 +4,26 @@ public class DiskPhysics : MonoBehaviour {
 
   private Rigidbody diskRb;
 
-  void Start() {
-    diskRb = GetComponent<Rigidbody>();
-    diskRb.isKinematic = true;
-  }
+  void Start() { diskRb = GetComponent<Rigidbody>(); }
 
-  void Update() {
+  void FixedUpdate() {
 
-    Vector3 velocity = Vector3.ProjectOnPlane(diskRb.linearVelocity, Vector3.up);
-    float speed = velocity.magnitude;
+    // Get velocity of disk projected on the plane (not vertical velocity)
+    Vector3 velocity =
+        Vector3.ProjectOnPlane(diskRb.linearVelocity, Vector3.up);
 
-    Vector3 liftDir = Vector3.Cross(velocity.normalized, transform.right).normalized;
-    Vector3 lift = 0.1f * speed * speed * liftDir;
+    // Scale lift based on the angle of the frisbee (more when its straight up)
+    float angle = Vector3.Angle(transform.forward, velocity);
+    float liftScale = Mathf.Clamp01(Mathf.Cos(angle * Mathf.Deg2Rad));
 
-    float l = Mathf.Min(lift.magnitude, diskRb.mass * Physics.gravity.magnitude * 0.9f);
+    float liftMag = 0.9f * velocity.sqrMagnitude * liftScale;
+    Vector3 liftDir =
+        Vector3.Cross(velocity.normalized, transform.right).normalized;
 
-    lift = lift.normalized * l;
+    // Clamp the the magnitude so its not greater than 1.1*gravity
+    liftMag =
+        Mathf.Min(liftMag, diskRb.mass * Physics.gravity.magnitude * 1.1f);
 
-    diskRb.AddForce(lift);
+    diskRb.AddForce(liftMag * liftDir);
   }
 }
